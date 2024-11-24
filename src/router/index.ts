b9edb71250e6ae2cd3a3136285fc44aeca63a369
HDQOSH <osh1512652759@outlook.com>
2024-11-24 12:33:28 +0800

    路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
-import owe from "./modules/owe";
+import home from "@/router/modules/home";
+import personal from "@/router/modules/personal";
+import other from "@/router/modules/other";
+
 
 const router = createRouter({
   history: createWebHistory(), //可传参数，配置base路径，例如'/app'
-  routes: [...owe]
+  routes: [...home,...personal,...other]
 });
 
 export default router;
